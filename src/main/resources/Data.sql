f9ba1f399fa4eff0fbfc9d84798d3981bf8eb159
Pratibha <pratipw23@gmail.com>
2024-04-13 22:46:57 +0530

    ET: Added SQL queries to insert data into the tables

--- src/main/resources/Data.sql
+++ src/main/resources/Data.sql
@@ -1,8 +1,9 @@
-insert into user values (1,'neha', 'neha10@gmail.com')
-insert into user values (2,'vidya', 'vidya@john.com')
 
-insert into category values (1,'Travel')
-insert into category values (2,'Rent')
+insert into user values (1,'neha', 'neha10@gmail.com');
+insert into user values (2,'vidya', 'vidya@gmail.com');
 
-insert into expense values (100,'school trip','2024-04-13T17:08:43+00:00','India',1,1)
-insert into expense values (101,'Rent','2024-04-15T17:08:43+00:00','India',2,2)
+insert into category values (1,'Travel');
+insert into category values (2,'Rent');
+
+insert into expense values (10,'BusinessTrip','India',1,1);
+insert into expense values (11,'FordPayment','India',2,2);
