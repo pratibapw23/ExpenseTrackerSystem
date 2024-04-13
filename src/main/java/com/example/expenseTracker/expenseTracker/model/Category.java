4823a3b215592240e35f2560e370dd638e35f38a
Pratibha <pratipw23@gmail.com>
2024-04-13 16:45:03 +0530

    ET: Created Category bean in Model in MVC

---
+++ src/main/java/com/example/expenseTracker/expenseTracker/model/Category.java
@@ -0,0 +1,44 @@
+package com.example.expenseTracker.expenseTracker.model;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="category")
+public class Category {
+	
+	@Id
+	private long id;
+	
+	private String name;
+
+	public Category(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@ManyToOne(cascade=CascadeType.PERSIST)
+	private User user;
+	
+	
+}
