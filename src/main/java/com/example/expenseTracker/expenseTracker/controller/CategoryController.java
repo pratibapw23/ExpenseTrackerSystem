1a6fdb05f4b7c2543a67d3b317567dc9093acd16
Pratibha <pratipw23@gmail.com>
2024-04-17 21:20:55 +0530

    ET:Feature: Implemented 'Get Single category by ID' feature

--- src/main/java/com/example/expenseTracker/expenseTracker/controller/CategoryController.java
+++ src/main/java/com/example/expenseTracker/expenseTracker/controller/CategoryController.java
@@ -1,8 +1,12 @@
 package com.example.expenseTracker.expenseTracker.controller;
 
 import java.util.Collection;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +29,12 @@ public class CategoryController {
 	Collection<Category> categories(){
 		//Select * from categories
 		return categoryRepo.findAll();
+	}
+	
+	@GetMapping("/category/{id}")
+	ResponseEntity<?> getCategory(@PathVariable long id){
+		Optional<Category> cat=categoryRepo.findById(id);
+		return cat.map(response -> ResponseEntity.ok().body(response))
+				.orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 }
