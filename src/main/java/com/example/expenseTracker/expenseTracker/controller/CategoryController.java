7c41c64f6c982ac8a0eda37a8872942aa2576bdb
Pratibha <pratipw23@gmail.com>
2024-04-22 23:29:24 +0530

    ET: Implemented Delete functionality for category

--- src/main/java/com/example/expenseTracker/expenseTracker/controller/CategoryController.java
+++ src/main/java/com/example/expenseTracker/expenseTracker/controller/CategoryController.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -54,5 +55,11 @@ public class CategoryController {
 	ResponseEntity<Category>  updatecategory(@Validated @RequestBody Category category){
 		Category result=categoryRepo.save(category);
 		return ResponseEntity.ok().body(result);
+	}
+	
+	@DeleteMapping("/category/{id}")
+	ResponseEntity<?> deleteCategory(@PathVariable long id){
+		categoryRepo.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 }
